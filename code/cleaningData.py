17f4b01c9928bcae007e60a2edf8d054f067b509
aarithundi9 <mh7637@nyu.edu>
2025-01-28 21:23:40 -0500

    Update cleaningData.py

--- code/cleaningData.py
+++ code/cleaningData.py
@@ -9,12 +9,11 @@ df = pd.read_csv(data_path)
 
 # print(df.head())
 
-# Define the sports categories
 athletics_sports = ['Athletics']
 aquatics_sports = ['Swimming']
 gymnastics_sports = ['Gymnastics']
 
-# Add a new column to categorize sports into athletics, aquatics, gymnastics, or other
+# method to 
 def categorize_sport(sport):
     if sport in athletics_sports:
         return 'Athletics'
@@ -25,40 +24,38 @@ def categorize_sport(sport):
     else:
         return 'Other'
 
-# Apply the categorization function to the 'Sport' column
+# s
 df['Sport_Category'] = df['Sport'].apply(categorize_sport)
 
-# Group by 'Country' and 'Year' and count the occurrences of each category
+# finds the amount of athletes in each unique sports category and then separates the 4 categories
 grouped_df = df.groupby(['Team', 'Year', 'Sport_Category']).size().unstack(fill_value=0)
-
-# Reset the index to make 'Country' and 'Year' columns
 grouped_df = grouped_df.reset_index()
 
-# Rename columns to match the desired format
-grouped_df.columns.name = None  # Remove the 'Sport_Category' index
+#renames the newly createed columns for each sport category
+grouped_df.columns.name = None  
 grouped_df = grouped_df.rename(columns={'Athletics': '# athletics athletes', 
                                          'Aquatics': '# aquatics athletes', 
                                          'Gymnastics': '# gymnastics athletes', 
                                          'Other': '# other athletes'})
 
-grouped_df = grouped_df[grouped_df['Year'] >= 1968]
 
-# Define the weights for each sport
-aquatic_weight = 2  # Example weight, adjust as necessary
-gymnastics_weight = 1  # Example weight, adjust as necessary
-athletics_weight = 2  # Example weight, adjust as necessary
-other_weight = 0.5  # Example weight, adjust as necessary
+grouped_df = grouped_df[grouped_df['Year'] >= 1964]
 
-# Calculate the weighted sum
+# weights for each sport 
+aquatic_weight = 2  
+gymnastics_weight = 1  
+athletics_weight = 2 
+other_weight = 0.5  
+
+# create the weighted athletes column based on weights
 grouped_df['Weighted_Athletes'] = (grouped_df['# aquatics athletes'] * aquatic_weight + 
                                     grouped_df['# gymnastics athletes'] * gymnastics_weight + 
                                     grouped_df['# athletics athletes'] * athletics_weight + 
                                     grouped_df['# other athletes'] * other_weight)
 
-# Display the resulting dataframe
 
 grouped_df.rename(columns={'Team': 'Country_Name'}, inplace=True)
 
-print(grouped_df)
+# print(grouped_df)
 
-grouped_df.to_csv("C:/Users/aarit/Downloads/newDataSet.csv")
+# grouped_df.to_csv("C:/Users/aarit/Downloads/newDataSet.csv")
